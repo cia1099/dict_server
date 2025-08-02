fdb2cd1911ea6ef56b877b69f8042c33e4f1e791
Otto@M4Pro <cia1099@gmail.com>
2025-08-02 18:59:31 +0800

    descripted docker tag and modified Dockerfile image

--- docker/cmd.sh
+++ docker/cmd.sh
@@ -5,6 +5,10 @@ cd ~/project/dict_server
 docker build --platform linux/arm64 -f docker/Dockerfile -t dict_server:arm64 .
 docker build --platform linux/amd64 -f docker/Dockerfile -t dict_server .
 
+# tag repository
+docker tag dict_server asia-east1-docker.pkg.dev/china-wall-over/dict-server/dict_server:v1.0
+docker tag dict_server cia1099/dict_server:v1.0
+
 # run container
 docker run --name=dict_server --privileged -d --tmpfs /run --tmpfs /run/lock -p 22:22 -p 80:80 -p 443:443 -p 8866:8866 dict_server
 docker run --name=dict_test -ti --rm --privileged -d --tmpfs /run --tmpfs /run/lock -p 8866:8866 dict_server
