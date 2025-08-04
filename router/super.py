7e8f28f03a67fbfaf881ac244e16457ad929dd36
Otto@M4Pro <cia1099@gmail.com>
2025-08-04 19:06:10 +0800

    replaced background task with Process

--- router/super.py
+++ router/super.py
@@ -1,7 +1,8 @@
+from multiprocessing import Process
 from typing import Annotated
 from firebase_admin import credentials
 from config import config
-from fastapi import APIRouter, BackgroundTasks, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import PlainTextResponse
 from fastapi.security import OAuth2PasswordBearer
 from firebase.helper import clear_expirations
@@ -14,10 +15,11 @@ router = APIRouter(prefix="/admin")
 )
 async def clear_anonymous(
     admin: Annotated[str, Depends(OAuth2PasswordBearer(tokenUrl=""))],
-    bg_task: BackgroundTasks,
 ):
     if admin != config.FIREBASE_ADMIN:
         raise HTTPException(status_code=400, detail="Permission deny")
     cred = credentials.Certificate(admin)
-    bg_task.add_task(clear_expirations, cred)
-    return "Process trigger"
+    p = Process(target=clear_expirations, args=(cred,))
+    p.daemon = False
+    p.start()
+    return "Process is setting off"
